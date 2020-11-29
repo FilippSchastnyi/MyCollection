bd152b26a48704e4b63e4be407d7dac6bf786066
Филипп <filipp.schastny@ozitag.com>
2020-11-29 10:57:56 +0300

    accord is complite

--- app/static/js/src/widgets/accord.js
+++ app/static/js/src/widgets/accord.js
@@ -3,17 +3,21 @@ class Accord {
     this.$container = nodeElement
     this.$optionItem = this.$container.querySelectorAll('.js-accord-option')
 
-    this.whenOptionClick = this.whenOptionClick.bind(this)
+    this.optionClick = this.optionClick.bind(this)
     this.init()
   }
 
-  whenOptionClick() {
-    console.log('hello')
+  optionClick(e) {
+    const optionElement = e.target.closest('.js-accord-option')
+    if (!optionElement) return
+    else {
+      optionElement.classList.toggle('open')
+    }
   }
 
   init() {
     this.$optionItem.forEach(item => {
-      item.addEventListener('click', this.whenOptionClick)
+      item.addEventListener('click', this.optionClick)
     })
   }
 }
